bc8b74d144fd50ccb49a69d5e7c8c5af5e54e379
Johan Robles <johanrobles66@yahoo.com>
2022-03-08 10:38:21 -0500

    Implementacion de pedirle a los jugadores 1 y 2 el valor de la celda que quieren cambiar y posteriormente cambiar ese valor de la celda por el correspondiente a cada jugardor  (X o O). Falta arreglar algunos Bugs.

--- Triki.py
+++ Triki.py
@@ -1,13 +1,33 @@
-modelo = [['|', 1, '|', 2, '|', 3, '|'],
+tablero = [['|', 1, '|', 2, '|', 3, '|'],
           ['-------------------------'],
           ['|', 4, '|', 5, '|', 6, '|'],
           ['-------------------------'],
           ['|', 7, '|', 8, '|', 9, '|']]
 
-def mostrar_triqui(modelo):
-    for linea in modelo:
+def mostrar_triqui(tablero):
+    for linea in tablero:
         for elemento in linea:
             print(elemento, end='   ')
         print()
 
-mostrar_triqui(modelo)
+def cambiar_celda(tablero, celda_a_cambiar, valor_X_O):
+    for linea in tablero:
+        if celda_a_cambiar in linea:
+            ind = linea.index(celda_a_cambiar)
+            linea[ind] = valor_X_O
+            return tablero
+
+i = 1
+while i <= 9:
+    if i % 2 != 0:
+        print('Turno Del Jugador #1')
+        celda_a_cambiar = int(input('¿En cual celda deseas porner la X?: '))
+        cambiar_celda(tablero, celda_a_cambiar, 'X')
+        mostrar_triqui(tablero)
+    else:
+        print('Turno Del Jugador #2')
+        celda_a_cambiar = int(input('¿En cual celda deseas porner el O?: '))
+        cambiar_celda(tablero, celda_a_cambiar, 'O')
+        mostrar_triqui(tablero)
+
+    i += 1
